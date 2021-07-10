99e6737ca499da25c3ccab5a3395543a2ca006a8
yuuki.tsujimura <yuuki.tsujimura>
2021-07-10 21:18:57 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 辻村祐揮 on 2021/07/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
